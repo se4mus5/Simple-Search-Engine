d4e8ccc23c4d8b740d4a560eb47517a8ae5e7fab
se4mus <7493527+4seamus@users.noreply.github.com>
2023-05-01 10:51:25 -0700

    stage 3 complete

--- Simple Search Engine (Java)/task/src/search/Main.java
+++ Simple Search Engine (Java)/task/src/search/Main.java
@@ -6,9 +6,49 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> lines = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
+        List<String> lines = dataEntry(scanner);
+
+        int menuSelection;
+        do {
+            printMenu();
+            menuSelection = Integer.parseInt(scanner.nextLine());
+            switch (menuSelection) {
+                case 1 -> findPerson(lines, scanner);
+                case 2 -> printPeople(lines);
+                case 0 -> System.out.println("Bye!");
+                default -> System.out.println("Incorrect option! Try again.\n");
+            }
+        } while (menuSelection != 0);
+    }
+
+    private static void findPerson(List<String> lines, Scanner scanner) {
+        System.out.println("Enter a name or email to search all suitable people.");
+        String searchPattern = scanner.nextLine().trim();
+        List<String> matches = new ArrayList<>();
+        for (String line : lines) {
+            if (line.toLowerCase().contains(searchPattern.toLowerCase())) {
+                matches.add(line);
+            }
+        }
+        if (matches.size() > 0) {
+            //System.out.println("Found people:");
+            matches.forEach(System.out::println);
+        } else {
+            System.out.println("No matching people found.");
+        }
+        System.out.println();
+    }
+
+    private static void printPeople(List<String> lines) {
+        System.out.println("=== List of people ===");
+        lines.forEach(System.out::println);
+        System.out.println();
+    }
+
+    public static List<String> dataEntry(Scanner scanner) {
+        List<String> lines = new ArrayList<>();
         System.out.println("Enter the number of people:");
         int entries = Integer.parseInt(scanner.nextLine());
         System.out.println("Enter all people:");
@@ -17,25 +57,15 @@ public class Main {
         }
         System.out.println();
 
-        System.out.println("Enter the number of search queries:");
-        int queries = Integer.parseInt(scanner.nextLine());
-        System.out.println();
-        for (int i = 0; i < queries; i++) {
-            System.out.println("Enter data to search people:");
-            String searchPattern = scanner.nextLine().trim();
-            List<String> matches = new ArrayList<>();
-            for (String line : lines) {
-                if (line.toLowerCase().contains(searchPattern.toLowerCase())) {
-                    matches.add(line);
-                }
-            }
-            if (matches.size() > 0) {
-                System.out.println("Found people:");
-                matches.forEach(System.out::println);
-            } else {
-                System.out.println("No matching people found.");
-            }
-            System.out.println();
-        }
+        return lines;
+    }
+
+    public static void printMenu() {
+        System.out.println("""
+                === Menu ===
+                1. Find a person
+                2. Print all people
+                0. Exit
+                """);
     }
 }
