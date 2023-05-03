79f4b91766ebe6543cc08051c3ce130f050b776c
se4mus <7493527+4seamus@users.noreply.github.com>
2023-05-03 09:37:32 -0700

    stage 5 complete

--- Simple Search Engine (Java)/task/src/search/Main.java
+++ Simple Search Engine (Java)/task/src/search/Main.java
@@ -3,9 +3,7 @@ package search;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -13,13 +11,14 @@ public class Main {
 
         //List<String> lines = dataEntry(scanner);
         List<String> lines = Files.readAllLines(new File(args[1]).toPath());
+        Map<String, List<Integer>> invertedIndex = buildInvertedIndex(lines);
 
         int menuSelection;
         do {
             printMenu();
             menuSelection = Integer.parseInt(scanner.nextLine());
             switch (menuSelection) {
-                case 1 -> findPerson(lines, scanner);
+                case 1 -> findPerson(lines, scanner, invertedIndex);
                 case 2 -> printPeople(lines);
                 case 0 -> System.out.println("Bye!");
                 default -> System.out.println("Incorrect option! Try again.\n");
@@ -27,18 +26,33 @@ public class Main {
         } while (menuSelection != 0);
     }
 
-    private static void findPerson(List<String> lines, Scanner scanner) {
-        System.out.println("Enter a name or email to search all suitable people.");
-        String searchPattern = scanner.nextLine().trim();
-        List<String> matches = new ArrayList<>();
-        for (String line : lines) {
-            if (line.toLowerCase().contains(searchPattern.toLowerCase())) {
-                matches.add(line);
+    private static Map<String, List<Integer>> buildInvertedIndex(List<String> lines) {
+        Map<String, List<Integer>> invertedIndex = new HashMap<>();
+        for (int i = 0; i < lines.size(); i++) {
+            String[] words = lines.get(i).split("\\s+");
+            for (String word : words) {
+                if (invertedIndex.containsKey(word)) {
+                    invertedIndex.get(word).add(i);
+                } else {
+                    List<Integer> locations = new ArrayList<>();
+                    locations.add(i);
+                    invertedIndex.put(word, locations);
+                }
             }
         }
-        if (matches.size() > 0) {
-            //System.out.println("Found people:");
-            matches.forEach(System.out::println);
+        return invertedIndex;
+    }
+
+    private static void findPerson(List<String> lines, Scanner scanner, Map<String, List<Integer>> invertedIndex) {
+        System.out.println("Enter a name or email to search all suitable people.");
+        String searchPattern = scanner.nextLine().trim();
+
+        if (invertedIndex.containsKey(searchPattern)) {
+            System.out.printf("%d persons found:%n", invertedIndex.get(searchPattern).size());
+
+            for (int lineNumber : invertedIndex.get(searchPattern)) {
+                System.out.println(lines.get(lineNumber));
+            }
         } else {
             System.out.println("No matching people found.");
         }
